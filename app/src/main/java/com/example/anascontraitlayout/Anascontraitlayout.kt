8ffe194e7d6384b69519ef5528bc966776ceba07
alamsyaaaah <kipasangina009@gmail.com>
2023-09-21 13:02:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/anascontraitlayout/Anascontraitlayout.kt
@@ -0,0 +1,11 @@
+package com.example.anascontraitlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Anascontraitlayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anascontraitlayout)
+    }
+}
